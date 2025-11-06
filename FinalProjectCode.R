807e0ffe809be3e4e06e866e21c6818a9c365a66
BriannaHollister <briannahollisterrr@gmail.com>
2025-11-05 20:06:51 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -6,5 +6,6 @@ library(ggplot2)
 
 fry_data = read_excel("C:/Users/laure/Downloads/Food Truck Raw Data.xlsx")
 
-frey_data2 = read_excel("C:/Users/brian/Downloads/R for Business Analytics/Final Presentation/Food Truck Raw Data.xlsx")
+frey_data2 = read_excel("Food Truck Raw Data.xlsx")
+
 
