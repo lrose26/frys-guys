43df05d69f200cf5bca59ff28e77189ff37466c4
lrose26 <lauren.rose2604@gmail.com>
2025-11-05 20:23:59 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -6,7 +6,8 @@ library(ggplot2)
 library(janitor)
 
 raw_fry_data = read_excel("Edited Food Truck Raw Data.xlsx")
-tidy_fry_data = raw_fry_data |> select(!c("London", "Waterloo", "Toronto"))
+tidy_fry_data = raw_fry_data |> select(!c("London", "Waterloo", "Toronto")) |> janitor::clean_names()
+
 
 
 
