b040821cb1c37380770632735ea1095a0fdce68f
BriannaHollister <briannahollisterrr@gmail.com>
2025-11-19 13:46:43 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -9,6 +9,10 @@ library(ggpubr)
 raw_fry_data = read_excel("Edited Food Truck Raw Data.xlsx")
 tidy_fry_data = raw_fry_data |> select(!c("London", "Waterloo", "Toronto")) |> janitor::clean_names()
 
+tidy_fry_data = tidy_fry_data |> mutate(
+  total_sales = quantity_sold * price
+  )
+
 # no correlation 
 tidy_fry_data |> ggplot(aes(x = temperature, y = quantity_sold)) + geom_point() +
 geom_smooth(method = "lm")
@@ -39,10 +43,12 @@ facet_wrap(~city)
 # 0 = no festival, 1 = festival 
 tidy_fry_data |> ggplot(aes(x = festival, y = quantity_sold, fill = festival)) + geom_col() +
 facet_wrap(~city) 
+tidy_fry_data |> count(festival)
 
 # 0 = weekend, including Friday, 1 = weekday
 tidy_fry_data |> ggplot(aes(x = weekday, y = quantity_sold, fill = weekday)) + geom_col() +
 facet_wrap(~city)
+tidy_fry_data |> count(weekday)
 
 
 
