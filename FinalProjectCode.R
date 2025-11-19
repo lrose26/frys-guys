1f5d896b3932d5feaca5dd2e8a68f2813e6d90b5
BriannaHollister <briannahollisterrr@gmail.com>
2025-11-19 14:25:48 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -40,8 +40,11 @@ geom_smooth(method = "lm") + stat_cor(method = "pearson") +
 facet_wrap(~city)
 
 # use box plot to ensure no skewness
-tidy_fry_data |> ggplot(aes(x = factor(festival), y = quantity_sold, group = city)) + geom_boxplot() +
-facet_wrap(~city)
+tidy_fry_data |> ggplot(aes(x = festival, y = quantity_sold, fill = festival)) +
+  geom_boxplot() +
+  facet_wrap(~ city) +
+  scale_fill_manual(values = c("coral", "steelblue")) +
+  labs(x = "Festival Status", y = "Quantity Sold")
 
 
 
