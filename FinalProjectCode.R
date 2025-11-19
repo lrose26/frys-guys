c2719f1552ae5d12caf7cb166edebf1e2bc6b40e
BriannaHollister <briannahollisterrr@gmail.com>
2025-11-19 13:17:52 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(writexl)
 library(ggplot2)
 library(janitor)
+library(ggpubr)
 
 raw_fry_data = read_excel("Edited Food Truck Raw Data.xlsx")
 tidy_fry_data = raw_fry_data |> select(!c("London", "Waterloo", "Toronto")) |> janitor::clean_names()
@@ -17,11 +18,21 @@ tidy_fry_data |> ggplot(aes(x = temperature, y = quantity_sold)) + geom_point() 
 geom_smooth(method = "lm") +
 facet_wrap(~city)
 
+
 tidy_fry_data |> ggplot(aes(x = probability_of_precipitation, y = quantity_sold)) + geom_point() +
 geom_smooth(method = "lm")
 
 tidy_fry_data |> ggplot(aes(x = probability_of_precipitation, y = quantity_sold)) + geom_point() +
 geom_smooth(method = "lm") +
+facet_wrap(~city)
+
+# added correlation coefficients to verify
+tidy_fry_data |> ggplot(aes(x = temperature, y = quantity_sold)) + geom_point() +
+geom_smooth(method = "lm") + stat_cor(method = "person") +
+facet_wrap(~city)
+
+tidy_fry_data |> ggplot(aes(x = probability_of_precipitation, y = quantity_sold)) + geom_point() +
+geom_smooth(method = "lm") + stat_cor(method = "person") +
 facet_wrap(~city)
 
 
