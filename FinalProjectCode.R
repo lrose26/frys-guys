22fe6abdc9ed58a6f0fde623751a34d969cec094
BriannaHollister <briannahollisterrr@gmail.com>
2025-11-19 14:12:26 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -39,16 +39,9 @@ tidy_fry_data |> ggplot(aes(x = probability_of_precipitation, y = quantity_sold)
 geom_smooth(method = "lm") + stat_cor(method = "pearson") +
 facet_wrap(~city)
 
-# using geom_col() because of the 0 and 1 variable
-# 0 = no festival, 1 = festival 
-tidy_fry_data |> ggplot(aes(x = festival, y = quantity_sold, fill = festival)) + geom_col() +
-facet_wrap(~city) 
-tidy_fry_data |> count(festival)
-
-# 0 = weekend, including Friday, 1 = weekday
-tidy_fry_data |> ggplot(aes(x = weekday, y = quantity_sold, fill = weekday)) + geom_col() +
+# use box plot to ensure no skewness
+tidy_fry_data |> ggplot(aes(x = festival, y = quantity_sold)) + geom_boxplot() +
 facet_wrap(~city)
-tidy_fry_data |> count(weekday)
 
 
 
