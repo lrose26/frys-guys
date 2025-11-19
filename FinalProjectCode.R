a174ba6becf5d9aff7bd6332000db1b4b1a08fb3
BriannaHollister <briannahollisterrr@gmail.com>
2025-11-19 12:57:47 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -8,7 +8,9 @@ library(janitor)
 raw_fry_data = read_excel("Edited Food Truck Raw Data.xlsx")
 tidy_fry_data = raw_fry_data |> select(!c("London", "Waterloo", "Toronto")) |> janitor::clean_names()
 
-tidy_fry_data |> ggplot(aes(x = quantity_sold, y = temperature)) + geom_point()
+tidy_fry_data |> ggplot(aes(x = quantity_sold, y = temperature)) + geom_point() +
+geom_smooth(method = "lm")
+
 
 
 
