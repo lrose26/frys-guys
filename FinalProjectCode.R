aebd61a70fa016faec3a82f5f6f1bea72a139eff
lrose26 <lauren.rose2604@gmail.com>
2025-12-05 11:59:26 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -173,26 +173,27 @@ regression = lm(revenue ~ precip_percent + weekday + city, data = tidy_fry_data)
 
 ggplot(regression, aes(x = precip_percent, y = revenue, color = weekday)) +
 geom_point(alpha = 0.4, size = 2) +
-geom_line(aes(y = predict(model)), linewidth = 1.2) +
+geom_line(aes(y = predict(regression)), linewidth = 1.2) +
 facet_wrap(~ city, ncol = 2) +
-scale_color_manual(values = c("Weekend" = "#10b981", "Weekday" = "#f59e0b")) +
+scale_color_manual(values = c("Weekend" = "darkseagreen", "Weekday" = "darkorchid4")) +
 labs(
 title = "Precipitation vs Revenue by City",
 x = "Precipitation (%)",
 y = "Revenue ($)") 
 
-
-regression = lm(revenue ~ precip_percent + weekday + city, data = tidy_fry_data)
 
 ggplot(tidy_fry_data, aes(x = precip_percent, y = revenue, color = weekday)) +
 geom_point(alpha = 0.4, size = 2) +
-geom_line(aes(y = predict(model)), linewidth = 1.2) +
+geom_line(aes(y = predict(regression)), linewidth = 1.2) +
 facet_wrap(~ city, ncol = 2) +
-scale_color_manual(values = c("Weekend" = "#10b981", "Weekday" = "#f59e0b")) +
+scale_color_manual(values = c("Weekend" = "darkseagreen", "Weekday" = "darkorchid4")) +
 labs(
 title = "Precipitation vs Revenue by City",
 x = "Precipitation (%)",
-y = "Revenue ($)") 
+y = "Revenue ($)")
+
+
+
 
 
 
